d2ba3a7aee854376b7cc2cbdc251919998d808fd
Moriti-Ramolefo <ramolefo.moriti@gmail.com>
2022-11-10 15:26:22 +0200

    added embedded YT video

--- index.php
+++ index.php
@@ -15,7 +15,21 @@
 		background-size: cover !important;
 		backdrop-filter: blur(5px) !important;
 	}
-</style>
+
+	.iframe-container{
+		position: relative;
+		width: 100%;
+		padding-bottom: 56.25%; 
+		height: 0;
+	}
+	.iframe-container iframe{
+		position: absolute;
+		top:0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+	</style>
 
 <?php
 require 'authentication.php'; // admin authentication check 
@@ -39,34 +53,37 @@ include("include/login_header.php");
 
 ?>
 
-<style>
-	
-</style>
-
 <div class="row">
 	<div class="col-md-4 col-md-offset-3">
 		<!-- LOGO goes here -->
 		<center><h2 style="margin-top:1px;"></h2></center>
 		<!-- LOGO goes here -->
-		<div class="well" style="position:relative;top:20vh;">
-			<form class="form-horizontal form-custom-login" action="" method="POST">
-			  <div class="form-heading" style="background: transparent;">
-			    <h2 class="text-center" style="font-weight: bold; font-size: 30px; color: #000;">Login</h2>
-				<hr style="border: 5px solid #00b16a;">
-			  </div>
-			  
-			  <?php if(isset($info)){ ?>
-			  <h5 class="alert alert-danger"><?php echo $info; ?></h5>
-			  <?php } ?>
-			  <div class="form-group">
-			    <input type="text" class="form-control" placeholder="Username" name="username" required/>
-			  </div>
-			  <div class="form-group" ng-class="{'has-error': loginForm.password.$invalid && loginForm.password.$dirty, 'has-success': loginForm.password.$valid}">
-			    <input type="password" class="form-control" placeholder="Password" name="admin_password" required/>
-			  </div>
-			  <button type="submit" name="login_btn" class="btn btn-info pull-right" style="border: 0; border-radius: 10px;">Sign In</button>
-			</form>
+		<div class="">
+			<div class="iframe-container">
+			<iframe width="1000" height="400" src="https://www.youtube.com/embed/ly36kn0ug4k" title="YouTube video player" frameborder="0" 
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</div>
+			<div class="well" style="position:relative;top:20vh;">
+				<form class="form-horizontal form-custom-login" action="" method="POST">
+				<div class="form-heading" style="background: transparent;">
+					<h2 class="text-center" style="font-weight: bold; font-size: 30px; color: #000;">Login</h2>
+					<hr style="border: 5px solid #00b16a;">
+				</div>
+					
+				<?php if(isset($info)){ ?>
+				<h5 class="alert alert-danger"><?php echo $info; ?></h5>
+				<?php } ?>
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Username" name="username" required/>
+				</div>
+				<div class="form-group" ng-class="{'has-error': loginForm.password.$invalid && loginForm.password.$dirty, 'has-success': loginForm.password.$valid}">
+					<input type="password" class="form-control" placeholder="Password" name="admin_password" required/>
+				</div>
+				<button type="submit" name="login_btn" class="btn btn-info pull-right" style="border: 0; border-radius: 10px;">Sign In</button>
+				</form>			
+			</div>
 		</div>
+		
 	</div>
 </div>
 
